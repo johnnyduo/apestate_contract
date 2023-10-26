cbca230e5b345862e2a9202c8e04a506092114d8
Chomtana <Chomtana001@gmail.com>
2023-10-26 15:45:00 +0700

    Push contracts

---
+++ contracts/LandPriceExchange.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./LandPriceOracle.sol";
+
+uint256 constant PRICE_EXPIRATION = 3600;
+uint256 constant BUY_FEE = 30;
+uint256 constant BORROW_INTEREST = 100;
+uint256 constant BORROW_RATIO = 8000;
+
+// Assume we have unlimited liquidity
+contract LandPriceExchange is ERC1155("") {
+  LandPriceOracle public immutable oracle;
+  IERC20 public immutable baseAsset;
+
+  struct BorrowPosition {
+    address borrower;
+    uint256 landId;
+    uint256 asset;
+    uint256 share;
+  }
+
+  BorrowPosition[] public borrowPositions;
+
+  constructor(LandPriceOracle _oracle, IERC20 _baseAsset) {
+    oracle = _oracle;
+    baseAsset = _baseAsset;
+  }
+
+  // TODO: How to do BAND Protocol???
+  function THBUSD() public view returns(uint256) {
+    return 1 ether;
+  }
+
+  function priceExpiration(uint256 landId) public view returns(uint256) {
+    return oracle.latestFulfill(landId) + PRICE_EXPIRATION;
+  }
+
+  function isPriceExpired(uint256 landId) public view returns(bool) {
+    return block.timestamp > priceExpiration(landId);
+  }
+
+  function price(uint256 landId) public view returns(uint256) {
+    require(!isPriceExpired(landId), "Price Expired");
+    return oracle.price(landId);
+  }
+
+  function buy(uint256 landId, uint256 baseAmount) public {
+    uint256 landPrice = price(landId) * THBUSD() / 1 ether;
+    uint256 landShare = baseAmount * 1 ether / landPrice;
+
+    // Apply fee
+    landShare = landShare * (10000 - BUY_FEE) / 10000;
+
+    _mint(msg.sender, landId, landShare, "");
+    baseAsset.transferFrom(msg.sender, address(this), baseAmount);
+  }
+
+  function sell(uint256 landId, uint256 landShare) public {
+    uint256 landPrice = price(landId) * THBUSD() / 1 ether;
+    uint256 baseAmount = landShare * landPrice / 1 ether;
+
+    // Apply fee
+    baseAmount = baseAmount * (10000 - BUY_FEE) / 10000;
+
+    _burn(msg.sender, landId, landShare);
+    baseAsset.transfer(msg.sender, baseAmount);
+  }
+
+  function borrow(uint256 landId, uint256 baseAmount, uint256 borrowAmount) public returns(uint256 borrowId) {
+    uint256 landPrice = price(landId) * THBUSD() / 1 ether;
+    uint256 landShare = baseAmount * 1 ether / landPrice;
+
+    require(borrowAmount <= landShare * BORROW_RATIO / 10000, "Over borrow ratio");
+
+    _mint(msg.sender, landId, borrowAmount, "");
+    baseAsset.transferFrom(msg.sender, address(this), baseAmount);
+
+    borrowId = borrowPositions.length;
+
+    borrowPositions.push(
+      BorrowPosition({
+        borrower: msg.sender,
+        landId: landId,
+        asset: baseAmount,
+        share: borrowAmount
+      })
+    );
+  }
+
+  function borrowAndSell(uint256 landId, uint256 baseAmount, uint256 borrowAmount) public returns(uint256 borrowId) {
+    borrowId = borrow(landId, baseAmount, borrowAmount);
+    sell(landId, borrowAmount);
+  }
+
+  function redeem(uint256 borrowId) public {
+    BorrowPosition memory position = borrowPositions[borrowId];
+
+    require(msg.sender == position.borrower, "Not Borrower");
+
+    _burn(msg.sender, position.landId, position.share);
+    baseAsset.transfer(msg.sender, position.asset + position.asset * BORROW_INTEREST / 100);
+  }
+}
---
+++ contracts/LandPriceOracle.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+/**
+ * Request testnet LINK and ETH here: https://faucets.chain.link/
+ * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
+ */
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT WHICH USES HARDCODED VALUES FOR CLARITY.
+ * THIS EXAMPLE USES UN-AUDITED CODE.
+ * DO NOT USE THIS CODE IN PRODUCTION.
+ */
+
+contract LandPriceOracle is ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+    using Strings for uint256;
+
+    bytes32 private jobId;
+    uint256 private fee;
+
+    mapping(bytes32 => uint256) internal request2LandId;
+    mapping(uint256 => uint256) public latestFulfill;
+    mapping(uint256 => uint256) public price;
+
+    event RequestPrice(bytes32 indexed requestId, uint256 indexed landId, uint256 price);
+
+    /**
+     * @notice Initialize the link token and target oracle
+     *
+     * Sepolia Testnet details:
+     * Link Token: 0x779877A7B0D9E8603169DdbD7836e478b4624789
+     * Oracle: 0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD (Chainlink DevRel)
+     * jobId: ca98366cc7314957b8c012c72f05aeeb
+     *
+     */
+    constructor(
+      address _chainlinkToken,
+      address _chainlinkOracle
+    ) ConfirmedOwner(msg.sender) {
+        setChainlinkToken(_chainlinkToken);
+        setChainlinkOracle(_chainlinkOracle);
+        jobId = "ca98366cc7314957b8c012c72f05aeeb";
+        fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
+    }
+
+    /**
+     * Create a Chainlink request to retrieve API response, find the target
+     * data, then multiply by 1000000000000000000 (to remove decimal places from data).
+     */
+    function requestVolumeData(uint256 landId) public returns (bytes32 requestId) {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfill.selector
+        );
+
+        // Set the URL to perform the GET request on
+        req.add(
+            "get",
+            "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD"
+        );
+
+        // Set the path to find the desired data in the API response, where the response format is:
+        // {"RAW":
+        //   {"ETH":
+        //    {"USD":
+        //     {
+        //      "VOLUME24HOUR": xxx.xxx,
+        //     }
+        //    }
+        //   }
+        //  }
+        // request.add("path", "RAW.ETH.USD.VOLUME24HOUR"); // Chainlink nodes prior to 1.0.0 support this format
+        req.add("path", "RAW,ETH,USD,VOLUME24HOUR"); // Chainlink nodes 1.0.0 and later support this format
+
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int256 timesAmount = 10 ** 18;
+        req.addInt("times", timesAmount);
+
+        // Sends the request
+        requestId = sendChainlinkRequest(req, fee);
+
+        request2LandId[requestId] = landId;
+    }
+
+    /**
+     * Receive the response in the form of uint256
+     */
+    function fulfill(
+        bytes32 _requestId,
+        uint256 _price
+    ) public recordChainlinkFulfillment(_requestId) {
+        uint256 landId = request2LandId[_requestId];
+
+        price[landId] = _price;
+        latestFulfill[landId] = block.timestamp;
+
+        emit RequestPrice(_requestId, landId, _price);
+    }
+
+    /**
+     * Allow withdraw of Link tokens from the contract
+     */
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
+}
