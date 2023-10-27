3d454dfa1f3ced15197335ec0e05e56caffbfdb4
Chomtana <Chomtana001@gmail.com>
2023-10-27 17:15:51 +0700

    Implement network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY!
+const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY!
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.19",
@@ -11,6 +14,23 @@ const config: HardhatUserConfig = {
       },
     },
   },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://bscscan.com/
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  networks: {
+    bsc_testnet: {
+      url: "https://data-seed-prebsc-2-s3.binance.org:8545",
+      chainId: 97,
+      accounts: [ PRIVATE_KEY ]
+    },
+    optimism_testnet: {
+      url: "https://goerli.optimism.io",
+      chainId: 420,
+      accounts: [ PRIVATE_KEY ]
+    },
+  }
 };
 
 export default config;
