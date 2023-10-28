3856db7e3ad32075ac59edafdea9ac4f7e91ce49
Chomtana <Chomtana001@gmail.com>
2023-10-28 21:06:02 +0700

    Deploy contracts

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ The core oracle and exchange engine for Apestate, real estate exchange in the We
 
 * FakeUSDC: 0x5C3FD8a81162e526143dcad8DfcB76C7B583198a
 * LandPriceOracle: 0x5C1D8168cE281af7736d9a23119949b7CaA98BC7
-* LandPriceExchange: 0xa225af0045F2a583FcB67B84910Fd2e06A34EE0C
+* LandPriceExchange: 0xB0A6892f8b4eCdcc1B5ccAE88ac3dBbb321818A7
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,21 +4,19 @@ const CHAINLINK_TOKEN = "0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06"
 const CHAINLINK_ORACLE = "0xCC79157eb46F5624204f47AB42b3906cAA40eaB7"
 
 async function main() {
-  // const usdc = await ethers.deployContract("FakeUSDC");
-  // await usdc.waitForDeployment();
+  const usdc = await ethers.deployContract("FakeUSDC");
+  await usdc.waitForDeployment();
 
-  // console.log("FakeUSDC", await usdc.getAddress())
+  console.log("FakeUSDC", await usdc.getAddress())
 
-  // const oracle = await ethers.deployContract("LandPriceOracle", [ CHAINLINK_TOKEN, CHAINLINK_ORACLE ]);
-  // await oracle.waitForDeployment();
+  const oracle = await ethers.deployContract("LandPriceOracle", [ CHAINLINK_TOKEN, CHAINLINK_ORACLE ]);
+  await oracle.waitForDeployment();
 
-  // console.log("LandPriceOracle", await oracle.getAddress())
+  console.log("LandPriceOracle", await oracle.getAddress())
   
   const exchange = await ethers.deployContract("LandPriceExchange", [
-    // await oracle.getAddress(),
-    // await usdc.getAddress(),
-    "0x5C1D8168cE281af7736d9a23119949b7CaA98BC7",
-    "0x5C3FD8a81162e526143dcad8DfcB76C7B583198a",
+    await oracle.getAddress(),
+    await usdc.getAddress(),
   ]);
   await exchange.waitForDeployment();
 
