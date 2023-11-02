a7d4a2415fec2305b675b63bc86d9a2d09d5454f
Chomtana <Chomtana001@gmail.com>
2023-11-02 18:29:36 +0700

    Fix address

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -18,7 +18,7 @@ async function main() {
     // await oracle.getAddress(),
     // await usdc.getAddress(),
     '0x5C1D8168cE281af7736d9a23119949b7CaA98BC7',
-    '0xB0A6892f8b4eCdcc1B5ccAE88ac3dBbb321818A7',
+    '0x5C3FD8a81162e526143dcad8DfcB76C7B583198a',
   ]);
   await exchange.waitForDeployment();
 
